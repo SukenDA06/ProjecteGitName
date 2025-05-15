eaeb7e080938dfc86e8736e0e93b581247fb4544
Soufian <soufianbougrin44@gmail.com>
2025-05-15 09:08:25 +0200

    Commit inicial projecte

--- src/projectgitdam/ProjectGitDAM.java
+++ src/projectgitdam/ProjectGitDAM.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projectgitdam;
 
-/**
- *
- * @author sbo5815
- */
+import java.util.Scanner;
+
 public class ProjectGitDAM {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int total;
+        int primero;
+        int segundo;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Primer numero: ");
+        primero = entrada.nextInt();
+        
+        System.out.print("\n");
+        
+        System.out.print("Segundo numero: ");
+        segundo = entrada.nextInt();
+        
+        total = primero + segundo;
+        System.out.println("Numero total: "+total);
     }
     
 }
